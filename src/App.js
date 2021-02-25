76b705767a1729593e4435af29355b55da6f3c04
developer-dinesh <dirathore@bestpeers.com>
2021-02-25 16:21:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function App(){
+let curDate = new Date(2021, 2, 11, );
+curDate = curDate.getHours();
+let greeting = "";
+const cssStyle = { };
+
+
+if (curDate >= 1 && curDate < 12) {
+  greeting = 'Good Morning';
+  cssStyle.color = 'green';
+} else if (curDate >= 12 && curDate < 19) {
+  greeting = 'Good Afternoon';
+  cssStyle.color = 'orange';
+}
+else {
+  greeting = 'Good Night';
+  cssStyle.color = 'Black';
+}
+
+return(
+    <>
+    <div>
+      
+      <h1>
+        Hello sir, <span style={cssStyle}> {greeting}</span>
+      </h1>
+    </div>
+    </>
+);
+
+}
+
+export default App;
